79d5e7b09fe0cd2d26a6de878f43650bc9b73ed1
simonschweer <simonschweer@me.com>
2019-09-24 11:04:28 +0200

    Intial

---
+++ src/Providers/HelloWorldRouteServiceProvider.php
@@ -0,0 +1,21 @@
+<?php
+namespace HelloWorld\Providers;
+
+use Plenty\Plugin\RouteServiceProvider;
+use Plenty\Plugin\Routing\Router;
+
+/**
+ * Class HelloWorldRouteServiceProvider
+ * @package HelloWorld\Providers
+ */
+class HelloWorldRouteServiceProvider extends RouteServiceProvider
+{
+	/**
+	 * @param Router $router
+	 */
+	public function map(Router $router)
+	{
+		$router->get('', 'HelloWorld\Controllers\ContentController@sayHello');
+	}
+
+}
